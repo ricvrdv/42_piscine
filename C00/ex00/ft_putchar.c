3fd097023bf3e483a4137f6f42d87507cfdc8187
Ricardo Garcia <applecore@applecore.lan>
2024-09-09 22:34:08 +0100

    C00 - ex00

---
+++ C00/ex00/ft_putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+void    ft_putchar(char c);
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int main(void)
+{
+    char    c = 'R';
+
+    ft_putchar(c);
+    return (0);
+}
