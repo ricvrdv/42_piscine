bf0111cb5467c270ec2ea57441ebae1c7c7967b7
Ricardo Jorge Jesus Da Costa Garcia <rjesus-d@c1r8s8.42porto.com>
2024-09-10 17:31:13 +0100

    C02: ex00 ex01

---
+++ C02/ex00/ft_strcpy.c
@@ -0,0 +1,26 @@
+/*#include <stdio.h>*/
+
+char    *ft_strcpy(char *dest, char *src);
+
+char    *ft_strcpy(char *dest, char *src)
+{
+        char    *dest_start = dest;
+
+        while (*src)
+        {
+                *dest = *src;
+                dest++;
+                src++;
+        }
+        *dest = '\0';
+        return dest_start;
+}
+
+/*int   main(void)
+{
+        char    str1[30] = "Hello World!";
+        char    str2[30] = "Bye.";
+
+        ft_strcpy(str2, str1);
+        printf("str1: %s\nstr2: %s\n", str1, str2);
+}*/
