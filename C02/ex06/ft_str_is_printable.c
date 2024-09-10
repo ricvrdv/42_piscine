028f76e80be9b561a643bf1245883d6306a62c6f
Ricardo Jorge Jesus Da Costa Garcia <rjesus-d@c1r8s8.42porto.com>
2024-09-10 20:07:47 +0100

    C02: ex06

---
+++ C02/ex06/ft_str_is_printable.c
@@ -0,0 +1,33 @@
+//#include <stdio.h>
+
+int     ft_str_is_printable(char *str)
+{
+        int     i;
+
+        i = 0;
+        if (str[i] == '\0')
+        {
+                return (1);
+        }
+        while (str[i] != '\0')
+        {
+                if (!(str[i] >= 32 && str[i] <= 126))
+                {
+                        return (0);
+                }
+                i++;
+        }
+        return (1);
+}
+
+/*int   main(void)
+{
+        int     r1 = ft_str_is_printable("Pan1c!");
+        int     r2 = ft_str_is_printable("Pa\nic");
+        int     r3 = ft_str_is_printable("\t\n");
+        int     r4 = ft_str_is_printable("");
+
+        printf("str1: %i\nstr2: %i\nstr3: %i\nstr4: %i\n", r1, r2, r3, r4);
+
+        return (0);
+}*/
