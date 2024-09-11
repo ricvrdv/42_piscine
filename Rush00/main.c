bda9605b6559afd1920dea146ac6a66395af109a
Ricardo Jorge Jesus Da Costa Garcia <rjesus-d@c1r8s4.42porto.com>
2024-09-11 12:07:06 +0100

    +rush01

---
+++ Rush00/main.c
@@ -0,0 +1,13 @@
+void    rush(int x, int y);
+
+int     main(void)
+{
+        rush (5, 5);
+        rush (2, 2);
+        rush (-1, 1);
+        rush (5, -1);
+        rush (0, 5);
+        rush (5, 0);
+        rush (1, 1);
+        return (0);
+}
