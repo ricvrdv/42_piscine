9b7408cdc84bf326960591f729da2f6ae6318d70
Ricardo Jorge Jesus Da Costa Garcia <rjesus-d@c1r9s4.42porto.com>
2024-09-15 17:56:23 +0100

    C03: ex04

---
+++ C03/ex04/ft_strstr.c
@@ -0,0 +1,34 @@
+//#include <stdio.h>
+
+char    *ft_strstr(char *str, char *to_find)
+{
+        int     i;
+        int     j;
+
+        i = 0;
+        j = 0;
+        while (str[i] != '\0')
+        {
+                j = 0;
+                while (to_find[j] != '\0' && str[i + j] == to_find[j])
+                {
+                        j++;
+                }
+                if (to_find[j] == '\0')
+                {
+                        return (&str[i]);
+                }
+                i++;
+        }
+        return (NULL);
+}
+
+/*int   main(int argc, char *argv[])
+{
+        char    *substr;
+
+        printf("Haystack: %s\nNeedle: %s\n", argv[1], argv[2]);
+        substr = ft_strstr(argv[1], argv[2]);
+        printf("Substring: %s\n", substr);
+        return (0);
+}*/
